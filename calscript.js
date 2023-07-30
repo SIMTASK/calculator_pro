719537eb02b47c686cca954d4b9b4c50f716be9b
SIMTASK <138787642+SIMTASK@users.noreply.github.com>
2023-07-30 10:01:13 +0530

    Add files via upload

---
+++ calscript.js
@@ -0,0 +1,19 @@
+function appendCharacter(character) {
+    document.getElementById("result").value += character;
+  }
+  
+  function clearResult() {
+    document.getElementById("result").value = "";
+  }
+  
+  function deleteCharacter() {
+    var result = document.getElementById("result").value;
+    document.getElementById("result").value = result.slice(0, -1);
+  }
+  
+  function calculate() {
+    var result = document.getElementById("result").value;
+    var calculation = eval(result);
+    document.getElementById("result").value = calculation;
+  }
+  
